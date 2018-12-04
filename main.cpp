f434801d204686042f87ebd5c8212d757f20dd23
Ceereals <toccafondi2000@gmail.com>
2018-12-04 09:50:14 +0100

    aggiunta commenti al codice

--- main.cpp
+++ main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
 
 int main(){
-    for(int i=1; i<10; i++)
+    for(int i=1; i<10; i++)//ciclo con condizioni 
             std::cout<<i<<std::endl;
-    system("PAUSE");
+    system("PAUSE");//importante! senza di ciò il programma si chiude subito!
     return 0;
     }
